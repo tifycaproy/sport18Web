fcd96486b60c884249b8502f7c9411a7852f80f4
rubentorres26 <rubentorres26@VITP1420>
2019-02-12 15:58:24 -0400

    cambios menores

---
+++ app/Http/Controllers/Backend/EquiposController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Equipos;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EquiposController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Equipos  $equipos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Equipos $equipos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Equipos  $equipos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Equipos $equipos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Equipos  $equipos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Equipos $equipos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Equipos  $equipos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Equipos $equipos)
+    {
+        //
+    }
+}
