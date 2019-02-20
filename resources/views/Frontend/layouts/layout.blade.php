47df5b849e5ba79199ba04fd6696db47c94f835a
yosecc <yosec.cervino@gmail.com>
2019-02-20 18:37:42 -0400

    act

--- resources/views/Frontend/layouts/layout.blade.php
+++ resources/views/Frontend/layouts/layout.blade.php
@@ -15,7 +15,7 @@
 	{{-- <link rel="stylesheet" href="{{asset('css/normalize.css')}}"> --}}
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{asset('css/transitions.css')}}">
-	{{-- <link rel="stylesheet" href="{{asset('css/prettyPhoto.css')}}"> --}}
+	<link rel="stylesheet" href="{{asset('css/prettyPhoto.css')}}">
 	<link rel="stylesheet" href="{{asset('css/swiper.min.css')}}">
 	{{-- <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}"> --}}
 	<link rel="stylesheet" href="{{asset('css/animate.css')}}">
